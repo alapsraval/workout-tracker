d70e04e90ffe5ba9b9ed42e55fa3cd6bf91b870e
Alap Raval <alapraval@Alaps-MBP-2.T-mobile.com>
2021-08-22 00:51:46 -0500

    commented out process.exit for heroku deployment

--- seeders/seed.js
+++ seeders/seed.js
@@ -135,9 +135,7 @@ Workout.deleteMany({})
   .then(() => Workout.collection.insertMany(workoutSeed))
   .then((data) => {
     console.log(data.result.n + ' records inserted!');
-    //process.exit(0);
   })
   .catch((err) => {
     console.error(err);
-    process.exit(1);
   });
